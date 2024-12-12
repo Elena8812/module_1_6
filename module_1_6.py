f4b783194cc8b40070aeacca04086426191a9aee
Elena8812 <Len4ik8812@gmail.com>
2024-12-13 00:15:57 +1200

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,12 @@
+my_dict={'Elena': 1988, 'Lily': 2006, 'Olga': 2010}
+print(my_dict)
+print(my_dict ['Lily'])
+print(my_dict.get('Danil'))
+my_dict.update({'Danil': 2000, 'Egor': 1994})
+print(my_dict)
+my_set={'a', 'd', 'c', 'a', 'c', (1, 2, 3)}
+print(my_set)
+my_set.add('vitamins')
+print(my_set)
+my_set.remove('c')
+print(my_set)
